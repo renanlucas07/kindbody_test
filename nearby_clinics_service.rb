41ff806c0d5b00aacc3c7f72f99d6bd8f786aa12
Renan Lucas <renan.lucas07@gmail.com>
2021-12-12 18:21:19 -0300

    refactor name, create constructor, change logic, and refactor some methods

--- nearby_clinics_service.rb
+++ nearby_clinics_service.rb
@@ -1,91 +1,91 @@
-class ZipCodeEndpoint
-  class << self
-    def get_zip_in_radius(zipcode, radius, units = 'mile')
-      # 1. Get list of ZIP codes in radius from starting zip code point
-      extracted_zips_from_api = extract_zip_codes_from_api(zipcode, radius, units)
-      return [] unless extracted_zips_from_api
+# frozen_string_literal: true
 
-      # 2. find all ZIP codes in the DB that match the response, and sort by tier ASC
-      extracted_zips_from_db = extract_zip_codes_from_db(extracted_zips_from_api['zip_codes']).to_a
+require 'faraday'
+require 'json'
+require 'byebug'
+require 'yaml'
+require 'awesome_print'
 
-      # 3. Sort ZIP codes in the same tier by distance
-      sort_extracted_zips_from_db_by_distance(extracted_zips_from_api['zip_codes_with_distance'], extracted_zips_from_db)
+# Service to find clinics within given radius from given zip code
+class NearbyClinicsService
+  attr_reader :zipcode, :radius, :units
 
-      format_response(extracted_zips_from_db, extracted_zips_from_api['zip_codes_with_distance'])
+  def initialize(zipcode, radius, units = 'mile')
+    @zipcode = zipcode
+    @radius  = radius
+    @units   = units
+  end
+
+  def call
+    # 1. Get list of ZIP codes in radius from starting zip code point
+    extract_zip_codes_from_api
+    return [] unless @extracted_zips_from_api
+
+    # 2. find all ZIP codes in the DB that match the response, and sort by tier ASC
+    extracted_zips_from_db = extract_zip_codes_from_db
+
+    # 3. Format response then sort by tier and distance
+    formatted_response = format_response(extracted_zips_from_db)
+    formatted_response.sort_by { |z| [z[:tier], z[:distance]] }
+  end
+
+  private
+
+  def format_response(sorted_zips)
+    sorted_zips.map do |item|
+      {
+        name: item.name,
+        address: item.address,
+        city: item.city,
+        state: item.state,
+        distance: @extracted_zips_from_api[:zip_codes_with_distance][item.zipcode],
+        tier: item.tier,
+        contact_email: item.contact_email,
+        contact_name: item.contact_name
+      }
     end
+  end
 
-    private
+  def extract_zip_codes_from_api
+    call_zip_codes_api
+    return nil unless @zip_data
 
-    def format_response(sorted_zips, zip_codes_with_distance)
-      response = []
-      sorted_zips.each do |item|
-        tmp_item = {
-          name: item.name,
-          address: item.address,
-          city: item.city,
-          state: item.state,
-          distance: zip_codes_with_distance[item.zipcode],
-          tier: item.tier,
-          contact_email: item.contact_email,
-          contact_name: item.contact_name
+    zip_codes_distance = {}
+    @zip_data['zip_codes'].each { |z| zip_codes_distance[z['zip_code']] = z['distance'] }
+
+    @extracted_zips_from_api = {
+      zip_codes: @zip_data['zip_codes'].map(&->(z) { z['zip_code'] }),
+      zip_codes_with_distance: zip_codes_distance
+    }
+  end
+
+  def call_zip_codes_api
+    url = "https://www.zipcodeapi.com/rest/#{ENV['ZIP_CODE_API_KEY']}/radius.json/#{zipcode}/#{radius}/#{units}"
+    zip_response = Faraday.get(url)
+    return nil unless zip_response.success?
+
+    @zip_data = JSON.parse(zip_response.body)
+  end
+
+  def extract_zip_codes_from_db
+    zipcodes = @extracted_zips_from_api[:zip_codes]
+    tier_values = %w[A B C]
+    # PartnerClinic.where('zipcode IN (?)', zipcodes).where('tier IN (?)', tier_values).order('tier ASC')
+    YAML.load_file('clinics.yml').map do |item|
+      OpenStruct.new(
+        {
+          zipcode: item['zipcode'],
+          tier: item['tier'],
+          name: '',
+          address: '',
+          city: '',
+          state: '',
+          contact_email: '',
+          contact_name: ''
         }
-        response.push(tmp_item)
-      end
-      response
+      )
     end
-
-    def extract_zip_codes_from_api(zipcode, radius, units)
-      zip_data = call_zip_codes_api(zipcode, radius, units)
-      return nil unless zip_data
-
-      tmp_array_of_zips = { 'zip_codes' => [], 'zip_codes_with_distance' => {} }
-      zip_data['zip_codes'].each do |item|
-        tmp_array_of_zips['zip_codes'].push(item['zip_code'])
-        tmp_array_of_zips['zip_codes_with_distance'][item['zip_code']] = item['distance']
-      end
-      tmp_array_of_zips
-    end
-
-    def call_zip_codes_api(zipcode, radius, units)
-      url = "https://www.zipcodeapi.com/rest/#{ENV['ZIP_CODE_API_KEY']}/radius.json/#{zipcode}/#{radius}/#{units}"
-      zip_response = Faraday.send(:post, url)
-      return nil unless zip_response.success?
-
-      JSON.parse(zip_response.body)
-    end
-
-    def extract_zip_codes_from_db(zipcodes)
-      tier_values = ['A', 'B', 'C']
-      PartnerClinic.where('zipcode IN (?)', zipcodes).where('tier IN (?)', tier_values).order('tier ASC')
-    end
-
-    def sort_extracted_zips_from_db_by_distance(zip_codes_with_distance, zip_codes_from_db)
-      i = 0
-      tier_marker_begin = 0
-      while i < zip_codes_from_db.length
-        if (i == zip_codes_from_db.length - 1) || (zip_codes_from_db[i + 1].tier != zip_codes_from_db[i].tier)
-          (tier_marker_begin..i - 1).each do |j|
-            (tier_marker_begin..(i - 1 - j + tier_marker_begin)).each do |k|
-              if compare_zip_distances(zip_codes_from_db[k].zipcode, zip_codes_from_db[k + 1].zipcode, zip_codes_with_distance)
-                swap_zip_codes(k, k + 1, zip_codes_from_db)
-              end
-            end
-          end
-          tier_marker_begin = i + 1
-        end
-        i += 1
-      end
-    end
-
-    def compare_zip_distances(zip1, zip2, zip_codes_with_distance)
-      # use zip_codes as keys to get the distances from zip_codes_with_distance
-      zip_codes_with_distance[zip1] > zip_codes_with_distance[zip2]
-    end
-
-    def swap_zip_codes(index_src, index_dst, zip_codes)
-      tmp = zip_codes[index_src]
-      zip_codes[index_src] = zip_codes[index_dst]
-      zip_codes[index_dst] = tmp
-    end
+  end
+end
   end
 end
